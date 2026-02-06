96e41e9cbeb0f683adfdc31a1328e35e1624f54f
Meetkumar Mistry <meetkumarmistry25@gmail.com>
2026-02-06 11:03:03 +0530

    Final Commit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e719d874d92b86349985b2860dedb164084580b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9944f19ab76c835279b5942c2ae93485521b8264")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
